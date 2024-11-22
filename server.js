335963c4b88afaf9d64d93dc9eb742fa3bb2c6ec
SUBHAJIT KARMAKAR <subhajitk731@gmail.com>
2024-11-23 02:16:11 +0530

    added style

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const File = require("./models/Files");
 
 const app = express();
 app.use(express.urlencoded({extended: true}))
+app.use(express.static('views'))
 
 const upload = multer({dest: "upload"})
 
@@ -33,6 +34,7 @@ app.post("/upload",upload.single("file"),async (req,res) => {
         path: req.file.path,
         originalName: req.file.originalname
     }
+
     if(req.body.password !== null && req.body.password !== ""){
         fileData.password = await bcrypt.hash(req.body.password,10)
     }
@@ -70,7 +72,7 @@ app.route("/file/:id").get(handleDownload).post(handleDownload);
 
 async function handleDownload(req,res){
     const file = await File.findById(req.params.id);
-
+    // console.log(req.body);
     if(file.password != null){
         if(req.body.password == null){
             res.render("password");
